782520e3155aeb978ac0af01006112fb33194f25
Sergey Lubimov <siarhei_liubimau@epam.com>
2023-01-23 12:19:58 +0300

    docs: loadtest -n 1000 -c 100 resault

--- express_cluster.js
+++ express_cluster.js
@@ -12,28 +12,16 @@ app.get('/', (req, res) => {
       }
 
       res.send(`Server ${process.pid} send response OK`)
-      cluster.worker.kill()
 })
 
 if (cluster.isMaster) {
       for (let i = 1; i <= numCPU; i++) {
             cluster.fork()
       }
-      cluster.on('exit', (worker, code, signal) => {
-            console.log(`worker ${worker.process.pid} died: `)
-            
-            if (signal) {
-                  console.log(` killed by signal: ${signal}`)
-                } else if (code !== 0) {
-                  console.log(` exited with error code: ${code}`) // message after exit in setTimeout
-                }
-
+      cluster.on('exit', (worker) => {
+            console.log(`worker ${worker.process.pid} died`)
             cluster.fork()
       })
 } else {
-      setTimeout((function() { 
-            return process.exit(1); //process exited with code 1 nodejs 
-            }), 5000);
-
       app.listen(3000, () => console.log(`Server ${process.pid} started...`))
 }
